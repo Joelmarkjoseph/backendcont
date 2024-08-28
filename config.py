9bdf8fbc6a3570d4f51471ca5ef2079dad259d6f
Joel Mark Joseph S <joelmarkjoseph2004@gmail.com>
2024-08-29 04:49:17 +0530

    new commits

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-app.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///mydatabase.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://joel:0K8r39zuE0zWY5jti41Ukx1jLXsJfEAR@dpg-cr7qu3a3esus73c1221g-a.oregon-postgres.render.com/contdb"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-db=SQLAlchemy(app)
+db = SQLAlchemy(app)
